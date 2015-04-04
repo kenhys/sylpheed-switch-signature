d4275678b002ed5a32b3230c1aac5096ff822e18
HAYASHI Kentaro <kenhys@gmail.com>
2015-04-04 23:34:14 +0900

    Generate version in advance

--- autogen.sh
+++ autogen.sh
@@ -9,11 +9,11 @@ run()
     fi
 }
 
+if [ -d .git ]; then
+    git log --abbrev-commit --oneline -1 | cut -d' ' -f 1 > version
+fi
 run aclocal ${ACLOCAL_ARGS}
 run libtoolize --copy --force
 run autoheader
 run automake --add-missing --foreign --copy
 run autoconf
-if [ -d .git ]; then
-    git log --abbrev-commit --oneline -1 | cut -d' ' -f 1 > version
-fi
